8dd5f3d2b7d29fa1249c3e3d0ade7a1438baf9bf
chandranshh <chandranshyay@gmail.com>
2023-06-01 21:18:49 +0530

    made changes to support routes

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Create Express server
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+//test
+app.get(`/`, (req, res) => {
+  res.send("API is working");
+});
+
+// Connect to MongoDB
+require("./connection");
+
+// Routes
+app.use("/api/auth/register", require("./routes/userAuth/register"));
+
+//port listening
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
