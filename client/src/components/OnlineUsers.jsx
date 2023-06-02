c98f12dba88b30181aaf9ad70316f035655280ce
chandranshh <chandranshyay@gmail.com>
2023-06-03 00:39:21 +0530

    made chnages in the Chat.jsx to look for socket change for online user

--- client/src/components/OnlineUsers.jsx
+++ client/src/components/OnlineUsers.jsx
@@ -1,11 +1,9 @@
-import { useSelector } from "react-redux";
 import { fetchAllUsers } from "../controllers/chat/fetchUsers";
 import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
 
-function OnlineUsers() {
+function OnlineUsers(props) {
+  console.log(props.online);
   const [allUsers, setAllUsers] = useState([]);
-  const [onlineUsers, setOnlineUsers] = useState([]);
 
   useEffect(() => {
     const getAllUsers = async () => {
@@ -13,25 +11,6 @@ function OnlineUsers() {
       setAllUsers(users);
     };
     getAllUsers();
-  }, []);
-
-  // Socket.io for checking who's online
-  const senderData = useSelector((state) => state.senderData);
-  const socket = io("http://localhost:3001", { transports: ["websocket"] });
-
-  if (senderData) {
-    socket.emit("connected", {
-      _id: senderData?._id,
-      username: senderData?.username,
-      email: senderData?.email,
-    });
-  }
-
-  useEffect(() => {
-    socket.on("users", (users) => {
-      setOnlineUsers(users);
-    });
-    console.log(onlineUsers);
   }, []);
 
   return (
@@ -51,7 +30,7 @@ function OnlineUsers() {
         <div className="p-2 bg-slate-200 w-full text-center">
           <span>Online user(s)</span>
         </div>
-        {onlineUsers.map((user) => (
+        {props.online.map((user) => (
           <div className="p-2" key={user?._id}>
             {user?.username}
           </div>
