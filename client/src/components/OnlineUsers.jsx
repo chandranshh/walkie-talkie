fcd3e9414469008cfc16649e2bb0c45be6c5e949
chandranshh <chandranshyay@gmail.com>
2023-06-03 14:43:15 +0530

    now the users are getting updated correctly -error : need to refresh the page once

--- client/src/components/OnlineUsers.jsx
+++ client/src/components/OnlineUsers.jsx
@@ -1,9 +1,13 @@
-/* eslint-disable react/prop-types */
+import { useSelector } from "react-redux";
 import { fetchAllUsers } from "../controllers/chat/fetchUsers";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { socket } from "../socket";
 
 function OnlineUsers() {
   const [allUsers, setAllUsers] = useState([]);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const senderData = useSelector((state) => state.senderData);
+  const onlineUsersDisplay = useRef([]);
 
   useEffect(() => {
     const getAllUsers = async () => {
@@ -12,6 +16,36 @@ function OnlineUsers() {
     };
     getAllUsers();
   }, []);
+
+  useEffect(() => {
+    socket.emit("connected", senderData);
+  }, [senderData]);
+
+  useEffect(() => {
+    socket.on("users", (users) => {
+      setOnlineUsers(users);
+    });
+
+    socket.on("newUser", (user) => {
+      setOnlineUsers((prevUsers) => [...prevUsers, user]);
+    });
+
+    socket.on("userLeft", (userId) => {
+      setOnlineUsers((prevUsers) =>
+        prevUsers.filter((user) => user._id !== userId)
+      );
+    });
+
+    return () => {
+      socket.off("users");
+      socket.off("newUser");
+      socket.off("userLeft");
+    };
+  }, [socket]);
+
+  useEffect(() => {
+    onlineUsersDisplay.current = onlineUsers.map((user) => user);
+  }, [onlineUsers]);
 
   return (
     <div className="w-[30%] h-full">
@@ -30,11 +64,11 @@ function OnlineUsers() {
         <div className="p-2 bg-slate-200 w-full text-center">
           <span>Online user(s)</span>
         </div>
-        {/* {props.online.map((user) => (
+        {onlineUsersDisplay.current.map((user) => (
           <div className="p-2" key={user?._id}>
             {user?.username}
           </div>
-        ))} */}
+        ))}
       </div>
     </div>
   );
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,47 +1,13 @@
-import { useEffect, useState } from "react";
 import Chatbox from "../components/Chatbox";
 import OnlineUsers from "../components/OnlineUsers";
 import Sidebar from "../components/Sidebar";
-import { useSelector } from "react-redux";
-import { socket } from "../socket";
 
 function Chat() {
-  const [onlineUsers, setOnlineUsers] = useState([]);
-  const senderData = useSelector((state) => state.senderData);
-
-  useEffect(() => {
-    socket.emit("connected", senderData);
-  }, [socket]);
-
-  useEffect(() => {
-    socket.on("users", (users) => {
-      setOnlineUsers(users);
-    });
-
-    return () => {
-      socket.off("users");
-    };
-  }, [socket]);
-
-  useEffect(() => {
-    socket.on("newUser", (user) => {
-      setOnlineUsers(user);
-    });
-
-    return () => {
-      socket.off("newUser");
-    };
-  }, [socket]);
-
-  useEffect(() => {
-    console.log(onlineUsers);
-  }, [onlineUsers]);
-
   return (
     <div className="flex h-screen">
       <Sidebar />
       <Chatbox />
-      <OnlineUsers online={onlineUsers} />{" "}
+      <OnlineUsers />
       {/* Pass the updated onlineUsers to the OnlineUsers component */}
     </div>
   );
