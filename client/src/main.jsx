9e35afde148b8759a4d8252ce35e9beebe7debab
chandranshh <chandranshyay@gmail.com>
2023-06-04 19:06:41 +0530

    minor change for deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-window.SERVER = "http://localhost:3001";
+window.SERVER = "https://walkie-talkie-backend.vercel.app/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
