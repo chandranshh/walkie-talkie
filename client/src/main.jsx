214c0b5f4b0f0d6d8306ab442ebd202ddeb3af92
chandranshh <chandranshyay@gmail.com>
2023-06-04 19:42:16 +0530

    switch from vercel to adaptable.io - test

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-window.SERVER = "https://walkie-talkie-backend.vercel.app";
+window.SERVER = "https://walkie-talkie.adaptable.app/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = "https://walkie-talkie-backend.vercel.app";
+const URL = "https://walkie-talkie.adaptable.app/";
 
 export const socket = io(URL);
