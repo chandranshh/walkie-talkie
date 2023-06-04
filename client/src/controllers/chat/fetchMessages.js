d7392ec8b502cdc5f3a49c0b0989712b372081bd
chandranshh <chandranshyay@gmail.com>
2023-06-04 16:02:53 +0530

    added an helper to fetch messages from the backend

---
+++ client/src/controllers/chat/fetchMessages.js
@@ -0,0 +1,11 @@
+//this helper fetches the messages from the server and stores them in the state
+import axios from "axios";
+
+export const fetchMessages = async (roomId) => {
+  try {
+    const response = await axios.get(`${window.SERVER}/api/message/${roomId}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
