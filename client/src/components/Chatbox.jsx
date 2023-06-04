72b8485137d8bca3411a25f2770e8cd2b6d74812
chandranshh <chandranshyay@gmail.com>
2023-06-04 16:03:34 +0530

    made changes to support conditional rendering in further commit

--- client/src/components/Chatbox.jsx
+++ client/src/components/Chatbox.jsx
@@ -1,20 +1,37 @@
 import { useDispatch, useSelector } from "react-redux";
-import { checkConvo } from "../controllers/chat/checkConvo";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { fetchMessages } from "../controllers/chat/fetchMessages";
 
 function Chatbox() {
   const { receiverData, sideBarUser } = useSelector(
     (state) => state.receiverData
   );
-  const { roomData } = useSelector((state) => state); // Assuming roomData is stored directly in the state
-
+  const { roomData } = useSelector((state) => state);
   const dispatch = useDispatch();
-
   const senderData = useSelector((state) => state.senderData);
   const roomId = receiverData?.roomId || roomData?.roomId;
+
+  const [messages, setMessages] = useState([]);
+
   useEffect(() => {
     dispatch({ type: "setRoomData/useSetRoomData", payload: roomId });
   }, [roomId]);
+
+  const reduxRoomId = useSelector((state) => state.roomData.roomId);
+
+  useEffect(() => {
+    const fetchMessagesData = async () => {
+      const fetchedMessages = await fetchMessages(reduxRoomId);
+      setMessages((prevMessages) => [...prevMessages, ...fetchedMessages]);
+    };
+
+    fetchMessagesData();
+    setMessages([]);
+  }, [reduxRoomId]);
+
+  useEffect(() => {
+    console.log(messages);
+  }, [messages]);
 
   return (
     <div className="w-[80%] h-[97vh] bg-blue-100 mx-2 flex flex-col items-center mt-2">
@@ -27,14 +44,12 @@ function Chatbox() {
         <div className="w-full h-full mt-3">
           <div className="flex justify-start">
             <div className="bg-blue-500 max-w-[48%] m-2 p-2 rounded-md text-white">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
-              natus eos porro. Tenetur dicta ex velit illum minima quos officia?
+              Hi
             </div>
           </div>
           <div className="flex justify-end">
             <div className="bg-gray-200 max-w-[49%] m-2 p-2 rounded-md">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. A minus
-              neque, aperiam architecto suscipit officiis?
+              Shin-Sna
             </div>
           </div>
           <div className="flex justify-start">
