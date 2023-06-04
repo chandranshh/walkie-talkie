5df5dda5b8e2eaf0a074d754dfa72d02cc0b17a3
chandranshh <chandranshyay@gmail.com>
2023-06-04 23:17:25 +0530

    chnage backend url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = "http://localhost:3001";
+const URL = "https://walkie-talkie-backend.onrender.com";
 
 export const socket = io(URL);
